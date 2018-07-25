91b3016ff55fe7f1a7b5bbeabd8b4ba3f847d546
Fredrik Arvidsson <fresa@fresa.se>
2018-07-25 20:33:37 +0200

    move tests to separate project and use .net standard

--- tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
+++ tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
@@ -54,30 +54,33 @@ using Xunit;
 //}
 
 
-public class When_you_have_a_new_stack : SmokeTestSpecification
+namespace Test.It.With.Smoke.Tests.Using.Xunit.Tests
 {
-    Stack<string> stack = new Stack<string>();
-
-    //protected override void Given()
-    //{
-    //    stack = new Stack<string>();
-    //}
-
-    [SmokeTest]
-    public void it_should_be_empty()
+    public class When_you_have_a_new_stack : SmokeTestSpecification
     {
-        Assert.True(stack.Count == 0);
-    }
+        Stack<string> stack = new Stack<string>();
 
-    [SmokeTest]
-    public void should_not_allow_you_to_call_Pop()
-    {
-        Assert.Throws<InvalidOperationException>(() => stack.Pop());
-    }
+        //protected override void Given()
+        //{
+        //    stack = new Stack<string>();
+        //}
 
-    [SmokeTest]
-    public void Should_not_allow_you_to_call_Peek()
-    {
-        Assert.Throws<InvalidOperationException>(() => { string unused = stack.Peek(); });
+        [SmokeTest]
+        public void it_should_be_empty()
+        {
+            Assert.True(stack.Count == 0);
+        }
+
+        [SmokeTest]
+        public void should_not_allow_you_to_call_Pop()
+        {
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [SmokeTest]
+        public void Should_not_allow_you_to_call_Peek()
+        {
+            Assert.Throws<InvalidOperationException>(() => { string unused = stack.Peek(); });
+        }
     }
 }
