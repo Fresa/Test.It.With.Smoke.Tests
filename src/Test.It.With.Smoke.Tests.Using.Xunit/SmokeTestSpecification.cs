fb92b914ab143d2a468ae44077461d0e74ef9192
Fredrik Arvidsson <fresa@fresa.se>
2018-07-27 18:23:07 +0200

    create some different design approaches for chaining test specifications

--- src/Test.It.With.Smoke.Tests.Using.Xunit/SmokeTestSpecification.cs
+++ src/Test.It.With.Smoke.Tests.Using.Xunit/SmokeTestSpecification.cs
@@ -1,4 +1,5 @@
-﻿using Test.It.Specifications;
+﻿using System;
+using Test.It.Specifications;
 
 namespace Test.It.With.Smoke.Tests.Using.Xunit
 {
@@ -9,4 +10,119 @@ namespace Test.It.With.Smoke.Tests.Using.Xunit
             Setup();
         }
     }
+
+    public abstract class SmokeTestSpecification<TNext> : SmokeTestSpecification
+    where TNext : SmokeTestSpecification
+    {
+        protected abstract TNext Next();
+    }
+
+    public interface INext<TFrom, TTo> 
+        where TFrom : SmokeTestSpecification
+    where TTo: SmokeTestSpecification
+    {
+        TTo Next(TFrom from);
+    }
+
+    public interface IStart<T> where T : SmokeTestSpecification
+    {
+        T Start();
+    }
+
+    public interface IProcess
+    {
+        IStart<T> Start<T>() where T : SmokeTestSpecification;
+    }
+
+    public interface INext2<TFrom>
+        where TFrom : SmokeTestSpecification
+    {
+        INext2<TTo> Next<TTo>()
+            where TTo : SmokeTestSpecification;
+    }
+
+    public interface INext<TFrom>
+        where TFrom : SmokeTestSpecification
+    {
+        INext<TTo> Next<TTo>(Func<TFrom, TTo> from)
+            where TTo : SmokeTestSpecification;
+    }
+
+    public class NextBuilder<TFrom> : INext<TFrom>
+        where TFrom : SmokeTestSpecification
+    {
+        private readonly TFrom _from;
+
+        public NextBuilder(TFrom from)
+        {
+            _from = @from;
+        }
+
+        public INext<TTo> Next<TTo>(Func<TFrom, TTo> from)
+            where TTo : SmokeTestSpecification
+        {
+            var to = from(_from);
+            return new NextBuilder<TTo>(to);
+        }
+    }
+
+    public class NextBuilder2<TFrom> : INext2<TFrom>
+        where TFrom : SmokeTestSpecification
+    {
+        private readonly TFrom _from;
+
+        public NextBuilder2(TFrom from)
+        {
+            _from = @from;
+        }
+
+        public INext2<TTo> Next<TTo>() where TTo : SmokeTestSpecification
+        {
+            return new NextBuilder2<TTo>(Activator.CreateInstance<TTo>());
+        }
+    }
+
+    public abstract class Process
+    {
+        public INext<T> Start<T>(Func<T> fromFunc) 
+            where T : SmokeTestSpecification
+        {
+            var from = fromFunc();
+            return new NextBuilder<T>(from);
+        }
+    }
+
+    public abstract class Process2<TStart>
+        where TStart : SmokeTestSpecification
+    {
+        protected abstract void Start(INext<TStart> fromFunc);
+        
+        internal void StartInternal()
+        {
+            Start(new NextBuilder<TStart>(Activator.CreateInstance<TStart>()));
+        }
+    }
+
+    public abstract class Process3
+    {
+        public INext2<T> Start<T>()
+            where T : SmokeTestSpecification
+        {
+            return new NextBuilder2<T>(Activator.CreateInstance<T>());
+        }
+    }
+
+
+    public interface IFirst<T>
+        where T : SmokeTestSpecification
+    {
+
+    }
+
+    public interface IAnd<T>
+        where T : SmokeTestSpecification
+    {
+
+    }
+
 }
--- tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
+++ tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
@@ -59,13 +59,100 @@ namespace Test.It.With.Smoke.Tests.Using.Xunit.Tests
 {
     public partial class Given_a_new_stack
     {
-        public class When_adding_an_item : SmokeTestSpecification
+        // Retrieve forwarding data from properties (what ever access modifier) 
+        // Forward data through constructor. Match parameter name and type with argument name and type
+        public class First_When_adding_an_item_and_adding_it_again : 
+            IFirst<When_adding_an_item>,
+            IAnd<When_adding_an_item_again>
         {
-            Stack<string> _stack;
+        }
+
+        public class When_adding_an_item_and_adding_it_again_with_process3 : Process3
+        {
+            public When_adding_an_item_and_adding_it_again_with_process3()
+            {
+                Start<When_adding_an_item>()
+                    .Next<When_adding_an_item_again>();
+            }
+        }
+
+        public class When_adding_an_item_and_adding_it_again_with_process2 : Process2<When_adding_an_item>
+        {
+            protected override void Start(INext<When_adding_an_item> fromFunc)
+            {
+                fromFunc.Next(item => new When_adding_an_item_again(null));
+            }
+        }
+
+        public class When_adding_an_item_and_adding_it_again_with_process : Process
+        {
+            public When_adding_an_item_and_adding_it_again_with_process()
+            {
+                Start(() => new When_adding_an_item())
+                    .Next(item => new When_adding_an_item_again(null));
+            }
+        }
+
+        public class When_adding_an_item_and_adding_it_again : 
+            IStart<When_adding_an_item>,
+            INext<When_adding_an_item, When_adding_an_item_again>
+        {
+            public When_adding_an_item Start()
+            {
+                return new When_adding_an_item();
+            }
+
+            public When_adding_an_item_again Next(When_adding_an_item @from)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public class When_adding_an_item : SmokeTestSpecification<When_adding_an_item_again>
+        {
+            public Stack<string> _stack { get; private set; }
+
+            protected override When_adding_an_item_again Next()
+            {
+                return new When_adding_an_item_again(_stack);
+            }
 
             protected override void Given()
             {
                 _stack = new Stack<string>();
+            }
+
+            protected override void When()
+            {
+                _stack.Push("item1");
+            }
+
+            [SmokeTest]
+            public void It_should_have_one_item()
+            {
+                _stack.Count.Should().Be(1);
+            }
+
+            [SmokeTest]
+            public void It_should_be_possible_to_pop()
+            {
+                _stack.Pop().Should().Be("item1");
+            }
+
+            [SmokeTest]
+            public void It_should_have_no_more_items()
+            {
+                _stack.Should().BeEmpty();
+            }
+        }
+
+        public class When_adding_an_item_again : SmokeTestSpecification
+        {
+            Stack<string> _stack;
+
+            public When_adding_an_item_again(Stack<string> stack)
+            {
+                _stack = stack;
             }
 
             protected override void When()
