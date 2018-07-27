597daa92395589f1f1644e8d60105cfa50b3f91b
Fredrik Arvidsson <fresa@fresa.se>
2018-07-27 18:23:46 +0200

    remove unused commented code

--- tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
+++ tests/Test.It.With.Smoke.Tests.Using.Xunit.Tests/When_you_have_a_new_stack.cs
@@ -2,59 +2,8 @@
 using System.Collections.Generic;
 using FluentAssertions;
 using Test.It.With.Smoke.Tests.Using.Xunit;
-using Xunit;
 
 [assembly: SmokeTestFramework]
-
-//public class Class1
-//{
-//    [Theory]
-//    [ClassData(typeof(TestProvider))]
-//    public void TestProvider(Action test)
-//    {
-//        test();
-//    }
-//}
-
-//public class BarTestData : IEnumerable<object[]>
-//{
-//    public IEnumerator<object[]> GetEnumerator()
-//    {
-//        yield return new object[] { 1, 2 };
-//        yield return new object[] { -4, -6 };
-//        yield return new object[] { 2, 4 };
-//    }
-
-//    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-//}
-
-//public class Test1
-//{
-
-//    [Theory]
-//    [ClassData(typeof(BarTestData))]
-//    public void BarTest(int value1, int value2)
-//    {
-//        Assert.True(value1 + value2 < 7);
-//    }
-
-//    [Theory]
-//    [MemberData(nameof(BazTestData))]
-//    public void BazTest(int value1, int value2)
-//    {
-//        Assert.True(value1 + value2 < 7);
-//    }
-
-//    public static IEnumerable<object[]> BazTestData => new List<object[]>
-//    {
-//        new object[] {1, 2},
-//        new object[] {-4, -6},
-//        new object[] {2, 40},
-//    };
-
-//}
-
-
 namespace Test.It.With.Smoke.Tests.Using.Xunit.Tests
 {
     public partial class Given_a_new_stack
